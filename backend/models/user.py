3fe52fc73a0cc96a9cd33ce5c177b606fa00528b
Samee-ul-haq <samisamee110@gmail.com>
2026-07-29 22:32:28 +0530

    Minor bug

--- backend/models/user.py
+++ backend/models/user.py
@@ -12,7 +12,7 @@ class User(Base):
     username = Column(String,nullable=False)
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
-    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     workspaces =  relationship("Workspace",back_populates= "user", cascade = "all, delete-orphan")
 
