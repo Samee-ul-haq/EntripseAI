cdafcef7481cdcfec8990b7ab7c1953c0ee3bea3
Samee-ul-haq <samisamee110@gmail.com>
2026-07-30 11:02:33 +0530

    Added description to workspace model

--- backend/models/workspaces.py
+++ backend/models/workspaces.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from backend.database import Base
 
@@ -7,6 +7,7 @@ class Workspace(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    description = Column(Text, nullable=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
 
     user = relationship("User", back_populates="workspaces")
