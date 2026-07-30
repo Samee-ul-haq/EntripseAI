4d17c2d934195e42ae58620187010349ad566b7e
Samee-ul-haq <samisamee110@gmail.com>
2026-07-30 12:56:33 +0530

    Upadated models of conversation and messages

--- backend/models/conversation.py
+++ backend/models/conversation.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
 from sqlalchemy.orm import relationship
 from backend.database import Base
-
+from sqlalchemy.sql import func
 
 class Conversation(Base):
     __tablename__ = "conversations"
@@ -9,6 +9,12 @@ class Conversation(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     workspace_id = Column(Integer, ForeignKey("workspaces.id"), nullable=False)
+    content = Column(Text , nullable=True)
+    
+    #Timestamps
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+    
 
     workspace = relationship("Workspace", back_populates="conversations")
     messages  =  relationship("Message",  back_populates="conversation", cascade="all, delete-orphan")
--- backend/models/message.py
+++ backend/models/message.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
 from backend.database import Base
 
@@ -7,8 +8,16 @@ class Message(Base):
     __tablename__ = "messages"
 
     id = Column(Integer, primary_key=True)
-    content = Column(String, nullable=False)
-    role = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    role = Column(String, nullable = False)
+    model_name = Column(String, nullable = True)
+    tokens_used = Column(int , nullable = True)
+    latency_ms = Column(int, nullable = True)
+
+    #Timestamps
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+    
 
     conversation_id = Column(Integer, ForeignKey("conversations.id"),nullable=False)
 
